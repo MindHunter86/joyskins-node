6db325f31e5a23439cea908ad246040e884cdabd
StringNick <wc3info@yandex.ru>
2016-06-24 21:10:57 +0300

    Отправка админу комисии, добавлен config,

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ var redisClient = redis.createClient(),
     client = redis.createClient();
 
 bot.init(redis, io, requestify);
-shop.init(redis, requestify);
+//shop.init(redis, requestify);
 server.listen(8080, '127.0.0.1');
 
 console.log('Server started on ' + config.domain + ':5000');
