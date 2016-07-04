93496b2bd5dc39544662f695bc872336df291556
StringNick <wc3info@yandex.ru>
2016-07-04 05:43:59 +0300

    Откл. бота и шопа

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ var auth = require('http-auth'),
     io      = require('socket.io')(server),
     redis   = require('redis'),
     requestify   = require('requestify'),
-    bot     = require('./bot.js'),
-    shop     = require('./shop.js'),
+   // bot     = require('./bot.js'),
+   // shop     = require('./shop.js'),
     duel = require('./duel.js');
 
 var redisClient = redis.createClient(),
