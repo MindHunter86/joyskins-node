e7fe1683fa2495d8507c6d6df044927e2366cd25
StringNick <wc3info@yandex.ru>
2016-07-07 19:26:11 +0300

    setPrizeStatus

--- duel.js
+++ duel.js
@@ -288,9 +288,7 @@ var setReceiveStatus = function(item,status,items){
             status: status,
             items: JSON.stringify(items)
         })
-        .then(function(response) {
-            console.log(JSON.stringify(items));
-        },function(response){
+        .then(function(response) {},function(response){
             console.tag('SteamBotDuel').error('Something wrong with setItemStatus. Retry...');
             setTimeout(function(){setPrizeStatus()}, 1000);
         });
