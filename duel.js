bd0ac56ecca59358670cdd69d0c222bc8cc34315
StringNick <wc3info@yandex.ru>
2016-07-18 11:49:22 +0300

    check

--- duel.js
+++ duel.js
@@ -560,4 +560,6 @@ setTimeout(function(){
 },50000);
 setInterval(function(){
     steamBotLogger('loggedOn:'+steamClient.loggedOn,' c0nnected:'+steamClient.connected);
+    if(!loggedOn)
+        relogin();
 },10000);
